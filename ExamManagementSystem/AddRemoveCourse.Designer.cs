5315ad4c8c94437dc0a95f5492d6a5aaa29e4a72
Asmaa Kamal <Asmaa Kamal@ASMAA-KAMAL>
2021-01-06 11:52:30 +0200

    commiting addremove course

--- ExamManagementSystem/AddRemoveCourse.Designer.cs
+++ ExamManagementSystem/AddRemoveCourse.Designer.cs
@@ -33,7 +33,14 @@ namespace ExamManagementSystem
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.buttonRemoveID = new System.Windows.Forms.Button();
-            this.textBoxCrsID = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txt_CourseName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Mgr_ID = new System.Windows.Forms.TextBox();
+            this.txt_Duration = new System.Windows.Forms.TextBox();
+            this.buttonUpdate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Admin
@@ -50,7 +57,7 @@ namespace ExamManagementSystem
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(169, 86);
+            this.label1.Location = new System.Drawing.Point(147, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(320, 23);
             this.label1.TabIndex = 15;
@@ -60,7 +67,7 @@ namespace ExamManagementSystem
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(356, 151);
+            this.label2.Location = new System.Drawing.Point(262, 118);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(111, 23);
             this.label2.TabIndex = 16;
@@ -68,20 +75,86 @@ namespace ExamManagementSystem
             // 
             // buttonRemoveID
             // 
+            this.buttonRemoveID.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonRemoveID.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonRemoveID.Location = new System.Drawing.Point(814, 147);
+            this.buttonRemoveID.Location = new System.Drawing.Point(1039, 93);
             this.buttonRemoveID.Name = "buttonRemoveID";
             this.buttonRemoveID.Size = new System.Drawing.Size(86, 29);
             this.buttonRemoveID.TabIndex = 18;
             this.buttonRemoveID.Text = "Remove";
             this.buttonRemoveID.UseVisualStyleBackColor = true;
+            this.buttonRemoveID.Click += new System.EventHandler(this.buttonRemoveID_Click);
             // 
-            // textBoxCrsID
+            // comboBox1
             // 
-            this.textBoxCrsID.Location = new System.Drawing.Point(489, 154);
-            this.textBoxCrsID.Name = "textBoxCrsID";
-            this.textBoxCrsID.Size = new System.Drawing.Size(280, 20);
-            this.textBoxCrsID.TabIndex = 17;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(392, 118);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txt_CourseName
+            // 
+            this.txt_CourseName.Location = new System.Drawing.Point(795, 83);
+            this.txt_CourseName.Name = "txt_CourseName";
+            this.txt_CourseName.Size = new System.Drawing.Size(195, 20);
+            this.txt_CourseName.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(615, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 23);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Course Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(615, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 23);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Manager ID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(615, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 23);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Duration:";
+            // 
+            // txt_Mgr_ID
+            // 
+            this.txt_Mgr_ID.Location = new System.Drawing.Point(795, 118);
+            this.txt_Mgr_ID.Name = "txt_Mgr_ID";
+            this.txt_Mgr_ID.Size = new System.Drawing.Size(195, 20);
+            this.txt_Mgr_ID.TabIndex = 24;
+            // 
+            // txt_Duration
+            // 
+            this.txt_Duration.Location = new System.Drawing.Point(795, 154);
+            this.txt_Duration.Name = "txt_Duration";
+            this.txt_Duration.Size = new System.Drawing.Size(195, 20);
+            this.txt_Duration.TabIndex = 25;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUpdate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.Location = new System.Drawing.Point(1039, 145);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(86, 29);
+            this.buttonUpdate.TabIndex = 26;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
             // 
             // AddRemoveCourse
             // 
@@ -89,8 +162,15 @@ namespace ExamManagementSystem
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(1192, 220);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.txt_Duration);
+            this.Controls.Add(this.txt_Mgr_ID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_CourseName);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonRemoveID);
-            this.Controls.Add(this.textBoxCrsID);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Admin);
@@ -98,6 +178,7 @@ namespace ExamManagementSystem
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AddRemoveCourse";
             this.Text = "AddRemoveCourse";
+            this.Load += new System.EventHandler(this.AddRemoveCourse_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,6 +189,13 @@ namespace ExamManagementSystem
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button buttonRemoveID;
-        private System.Windows.Forms.TextBox textBoxCrsID;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txt_CourseName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Mgr_ID;
+        private System.Windows.Forms.TextBox txt_Duration;
+        private System.Windows.Forms.Button buttonUpdate;
     }
 }
